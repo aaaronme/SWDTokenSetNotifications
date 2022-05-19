3171fda9c5c198641db41d5909afb6ee027f383b
Smooth1884 <smooth1884@gmail.com>
2022-05-19 10:09:01 +0800

    removed unused import

--- polygonGET.py
+++ polygonGET.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from genericpath import isdir, isfile
 import json
 import os
-from turtle import position
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
